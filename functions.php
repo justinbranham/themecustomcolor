ef8ccd7b7e2c2b7a7baf546974310aeff4bdb7f1
justinbranham <54411431+justinbranham@users.noreply.github.com>
2020-07-20 12:50:54 -0400

    Initial commit

---
+++ functions.php
@@ -0,0 +1,73 @@
+<?php
+
+function customcolor_theme_support(){
+  // Adds dynamic title tag support
+  add_theme_support('title-tag');
+  add_theme_support('custom-logo');
+  add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'customcolor_theme_support');
+
+function customcolor_menus(){
+
+  $locations = array(
+    'primary' => 'Desktop Primary Left Sidebar',
+    'footer' => 'Footer Menu Items'
+  );
+
+  register_nav_menus($locations);
+
+}
+
+add_action('init', 'customcolor_menus');
+
+function customcolor_register_styles(){
+
+  $version = wp_get_theme()->get( 'Version');
+  wp_enqueue_style('customcolor-style', get_template_directory_uri()."/style.css", array('customcolor-bootstrap'), $version, 'all');
+  wp_enqueue_style('customcolor-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+  wp_enqueue_style('customcolor-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'customcolor_register_styles');
+
+
+function customcolor_register_scripts(){
+
+  wp_enqueue_script('customcolor-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+  wp_enqueue_script('customcolor-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+  wp_enqueue_script('customcolor-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+  wp_enqueue_script('customcolor-main', get_template_directory_uri().'/assets/js/main.js', array(), '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'customcolor_register_scripts');
+
+function customcolor_widget_areas(){
+  register_sidebar(
+      array(
+        'before_title' => '',
+        'after_title' => '',
+        'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+        'after_widget' => '</ul>',
+        'name' => 'Sidebar Area',
+        'id' => 'sidebar-1',
+        'description' => 'Sidebar Widget Area',
+      )
+    );
+    register_sidebar(
+        array(
+          'before_title' => '',
+          'after_title' => '',
+          'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+          'after_widget' => '</ul>',
+          'name' => 'Footer Area',
+          'id' => 'footer-1',
+          'description' => 'Footer Widget Area',
+        )
+      );
+}
+
+add_action('widgets_init', 'customcolor_widget_areas');
+
+?>
